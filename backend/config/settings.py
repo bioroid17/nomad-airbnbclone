9c8536c2eae08ff2848e95b293103634f92d5f4f
bioroid17 <trace1997@ajou.ac.kr>
2024-08-10 21:47:42 +0900

    AUTHENTICATION - Environment Files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os, environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9%=x3scur(c@j1rlrli1o^3&!93kzuu7u-j-)+x7$4!nxb2@zo"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
