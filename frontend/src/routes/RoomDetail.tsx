6c2b3b33687ae42cb93490f9da8405047971a5ee
bioroid17 <trace1997@ajou.ac.kr>
2024-08-23 17:38:43 +0900

    REACT QUERY - Room Detail

---
+++ frontend/src/routes/RoomDetail.tsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+import { getRoom } from "../api";
+import { useQuery } from "@tanstack/react-query";
+
+export default function RoomDetail() {
+  const { roomPk } = useParams();
+  const { isLoading, data } = useQuery({
+    queryKey: [`room:${roomPk}`],
+    queryFn: getRoom,
+  });
+  console.log(data);
+
+  return <h1>hello</h1>;
+}
