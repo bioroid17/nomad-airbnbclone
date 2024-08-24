32d59519717a4cad38862fb021bc019817590b16
bioroid17 <trace1997@ajou.ac.kr>
2024-08-24 12:42:58 +0900

    REACT QUERY - Reviews

--- frontend/src/routes/RoomDetail.tsx
+++ frontend/src/routes/RoomDetail.tsx
@@ -1,21 +1,32 @@
 import { useParams } from "react-router-dom";
-import { getRoom } from "../api";
+import { getRoom, getRoomReviews } from "../api";
 import { useQuery } from "@tanstack/react-query";
-import { IRoomDetail } from "../types";
+import { IReview, IRoomDetail } from "../types";
 import {
+  Avatar,
   Box,
   Grid,
   GridItem,
   Heading,
+  HStack,
   Image,
   Skeleton,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
 
 export default function RoomDetail() {
   const { roomPk } = useParams();
   const { isLoading, data } = useQuery<IRoomDetail>({
     queryKey: [`rooms`, roomPk],
     queryFn: getRoom,
+  });
+  const { isLoading: isReviewsLoading, data: reviewsData } = useQuery<
+    IReview[]
+  >({
+    queryKey: [`rooms`, roomPk, `reviews`],
+    queryFn: getRoomReviews,
   });
 
   return (
@@ -50,6 +61,41 @@ export default function RoomDetail() {
           </GridItem>
         ))}
       </Grid>
+      <HStack width={"40%"} mt={10} justifyContent={"space-between"}>
+        <VStack alignItems={"flex-start"}>
+          <Skeleton isLoaded={!isLoading} height={"30px"}>
+            <Heading fontSize={"2xl"}>
+              House hosted by {data?.owner.name}
+            </Heading>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading}>
+            <HStack justifyContent={"flex-start"} w={"100%"}>
+              <Text>
+                {data?.toilets} toilet{data?.toilets === 1 ? "" : "s"}
+              </Text>
+              <Text>•</Text>
+              <Text>
+                {data?.rooms} room{data?.rooms === 1 ? "" : "s"}
+              </Text>
+            </HStack>
+          </Skeleton>
+        </VStack>
+        <Avatar name={data?.owner.name} size={"xl"} src={data?.owner.avatar} />
+      </HStack>
+      <Box mt={10}>
+        <Heading w={"20%"} mb={5} fontSize={"2xl"}>
+          <Skeleton isLoaded={!isReviewsLoading}>
+            <HStack>
+              <FaStar /> <Text>{data?.rating}</Text>
+              <Text>•</Text>
+              <Text>
+                {reviewsData?.length} review
+                {reviewsData?.length === 1 ? "" : "s"}
+              </Text>
+            </HStack>
+          </Skeleton>
+        </Heading>
+      </Box>
     </Box>
   );
 }
