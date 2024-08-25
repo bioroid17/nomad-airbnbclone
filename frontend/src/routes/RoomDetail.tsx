b8f58416d00ca287a7290848599467c6b7a61a26
bioroid17 <trace1997@ajou.ac.kr>
2024-08-25 15:05:58 +0900

    REACT QUERY - Conclusions

--- frontend/src/routes/RoomDetail.tsx
+++ frontend/src/routes/RoomDetail.tsx
@@ -5,6 +5,7 @@ import { IReview, IRoomDetail } from "../types";
 import {
   Avatar,
   Box,
+  Container,
   Grid,
   GridItem,
   Heading,
@@ -95,6 +96,31 @@ export default function RoomDetail() {
             </HStack>
           </Skeleton>
         </Heading>
+        <Container mt={16} maxW="container.lg" marginX={"none"}>
+          <Skeleton h={"30vh"} isLoaded={!isReviewsLoading}>
+            <Grid gap={10} templateColumns={"1fr 1fr"}>
+              {reviewsData?.map((review, index) => (
+                <VStack alignItems={"flex-start"} key={index}>
+                  <HStack>
+                    <Avatar
+                      name={review.user.name}
+                      src={review.user.avatar}
+                      size={"md"}
+                    />
+                    <VStack spacing={0} alignItems={"flex-start"}>
+                      <Heading fontSize={"md"}>{review.user.name}</Heading>
+                      <HStack spacing={1}>
+                        <FaStar size={"12px"} />
+                        <Text>{review.rating}</Text>
+                      </HStack>
+                    </VStack>
+                  </HStack>
+                  <Text>{review.payload}</Text>
+                </VStack>
+              ))}
+            </Grid>
+          </Skeleton>
+        </Container>
       </Box>
     </Box>
   );
