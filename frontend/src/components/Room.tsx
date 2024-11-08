a333959548c0fdd0930780f14e6d18cdf98fb2f6
bioroid17 <trace1997@ajou.ac.kr>
2024-11-09 02:53:16 +0900

    UPLOAD ROOM - Upload Form

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -8,8 +8,9 @@ import {
   useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
-import { FaRegHeart, FaStar } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React from "react";
+import { FaCamera, FaRegHeart, FaStar } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 
 interface IRoomProps {
   imageUrl: string;
@@ -19,6 +20,7 @@ interface IRoomProps {
   country: string;
   price: number;
   pk: number;
+  isOwner: boolean;
 }
 
 export default function Room({
@@ -29,13 +31,29 @@ export default function Room({
   city,
   country,
   price,
+  isOwner,
 }: IRoomProps) {
   const gray = useColorModeValue("gray.600", "gray.300");
+  const navigate = useNavigate();
+  const onCameraClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    navigate(`/rooms/${pk}/photos`);
+  };
   return (
     <Link to={`/rooms/${pk}`}>
       <VStack alignItems={"flex-start"}>
-        <Box position={"relative"} overflow={"hidden"} mb={3} rounded={"2xl"}>
-          <Image minH={"280"} src={imageUrl} />
+        <Box
+          w="100%"
+          position={"relative"}
+          overflow={"hidden"}
+          mb={3}
+          rounded={"2xl"}
+        >
+          {imageUrl ? (
+            <Image minH="280" src={imageUrl} />
+          ) : (
+            <Box minH="280px" h="100%" w="100%" p={10} bg="green.400" />
+          )}
           <Button
             variant={"unstyled"}
             cursor={"pointer"}
@@ -43,8 +61,13 @@ export default function Room({
             position={"absolute"}
             top={0}
             right={0}
+            onClick={onCameraClick}
           >
-            <FaRegHeart size={"20px"} />
+            {isOwner ? (
+              <FaCamera size={"20px"} />
+            ) : (
+              <FaRegHeart size={"20px"} />
+            )}
           </Button>
         </Box>
         <Box>
